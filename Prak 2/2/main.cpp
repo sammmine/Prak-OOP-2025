2d14adfb984fb77903e6c03536e63c6aa32544a8
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2025-03-25 11:29:14 +0700

    refactor: masih ada yang keliru pake ref atau pure instance

--- Prak 2/2/main.cpp
+++ Prak 2/2/main.cpp
@@ -2,23 +2,40 @@
 #include "Dragon.hpp"
 
 int main() {
-    Dragon d("Draco"); // A creature has been born! // A dragon has been born!
-    d.speak(); // I am the mighty dragon Draco! Beware of my flames!
+    Dragon d("Draco"); 
+    d.speak(); 
     d.wound();
     d.wound();
-    d.print(); // Base Health: 100; Additional Health: 80
-    d.speak(); // I am the mighty dragon Draco! Beware of my flames!
+    d.print(); 
 
-    Creature &dRef = d;
-    dRef.wound();
-    dRef.print(); // Base Health: 90
-    d.print(); // Base Health: 90; Additional Health: 80
+    Creature *dRef = &d; // Polymorphism (banyak bentuk) -> untuk merujuk ke objek dari kelas turunan,
+                         // tetapi pemanggilan fungsi virtual akan mengeksekusi metode yg sesuai dengan tipe aktual objek,
+                         // bukan tipe referensinya.
+    dRef->speak(); // harus virtual, gak bisa d.speak();
+    dRef->wound();
+    dRef->print(); 
+    d.print(); 
 
-    Creature dRefRef = dRef;
-    dRefRef.speak(); // I am a mystical creature named Draco!
-    dRef.wound();
-    dRef.print(); // Base Health: 80
-    d.print(); // Base Health: 90; Additional Health: 80
+    Creature dPure = d; // Slicing -> masalah yang terjadi ketika objek kelas turunan ditugaskan ke kelas dasar
+                        // akibatnya adalah bagian spesifik dari kelas turunan terpotong
+                        // dan hanya bisa mengakses bagian kelas dasar.
+                        // Di sini, dPure menjadi instance murni, sehingga ia hanya memiliki properti dan metode Creature.
+    dPure.speak(); 
+    dPure.wound(); // gak bisa dRef->wound()
+    dPure.print(); // gak bisa dRef->print()
+    d.print(); 
 
     return 0;
 }
+
+// Output:
+    // A creature has been born!
+    // A dragon has been born!
+    // I am the mighty dragon Draco! Beware of my flames!
+    // Base Health: 100; Additional Health: 80
+    // I am the mighty dragon Draco! Beware of my flames!
+    // Base Health: 90
+    // Base Health: 90; Additional Health: 80
+    // I am a mystical creature named Draco!
+    // Base Health: 80
+    // Base Health: 90; Additional Health: 80
